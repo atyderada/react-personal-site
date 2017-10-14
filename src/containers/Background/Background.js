48b6ad8c1e0f3dedf4478eacc56b16b0c45234a7
Anthony De Rada <anthonyderada@Anthonys-MacBook-Pro.local>
2017-10-14 13:21:38 -0700

    Cleaned up project

---
+++ src/containers/Background/Background.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import * as THREE from 'three';
+
+export default class Scene extends Component {
+  constructor(props) {
+    super(props)
+
+    this.start = this.start.bind(this)
+    this.stop = this.stop.bind(this)
+    this.animate = this.animate.bind(this)
+  }
+
+  componentDidMount() {
+    const width = this.mount.clientWidth
+    const height = this.mount.clientHeight
+
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      70, // vertical field of view (angle)
+      width / height, // ratio
+      15, // near plane (z distance)
+      10000 // far plane (z distance)
+    )
+
+    const renderer = new THREE.WebGLRenderer();
+
+  	// the camera starts at 0,0,0 so pull it back
+  	camera.position.z = 1100;
+
+  	// start the renderer - set the clear colour
+  	// to a full black
+    renderer.setClearColor('#FFFFFF')
+    renderer.setSize(width, height)
+
+
+    //This will add a starfield to the background of a scene
+    var starsGeometry = new THREE.Geometry();
+
+    for ( var i = 0; i < 420; i ++ ) {
+
+      var vertex = new THREE.Vector3();
+					vertex.x = 2000 * Math.random() - 1000;
+					vertex.y = 2000 * Math.random() - 1000;
+					vertex.z = 2000 * Math.random() - 1000;
+
+    	starsGeometry.vertices.push( vertex );
+    }
+
+    var material = new THREE.PointsMaterial( { size: 2.2, sizeAttenuation: false, alphaTest: 0.5, transparent: true } );
+		material.color.setHSL( 1.0, 0.3, 0.7 );
+    var starsMaterial = new THREE.PointsMaterial( { color: 0x777777 } );
+
+    var starField = new THREE.Points( starsGeometry, material );
+
+    scene.add( starField );
+
+    var light = new THREE.DirectionalLight( 0xffffff, 1 );
+		light.position.set( 1, 1, 1 ).normalize();
+		scene.add( light );
+
+    this.scene = scene
+    this.camera = camera
+    this.renderer = renderer
+    this.starField = starField;
+    this.material = starsMaterial
+
+    this.mount.appendChild(this.renderer.domElement)
+    this.start()
+  }
+
+  componentWillUnmount() {
+    this.stop()
+    this.mount.removeChild(this.renderer.domElement)
+  }
+
+  start() {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate)
+    }
+  }
+
+  // animation loop
+  animate() {
+
+    // animate
+    this.starField.rotation.y += 0.01;
+    this.starField.rotation.x += 0.003;
+    this.starField.rotation.z += 0.008;
+    this.renderScene()
+    this.frameId = window.requestAnimationFrame(this.animate)
+  }
+
+  stop() {
+    cancelAnimationFrame(this.frameId)
+  }
+
+  renderScene() {
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'fixed',
+          top: 0,
+          zIndex: 1
+        }}
+        ref={(mount) => { this.mount = mount }}
+      />
+    )
+  }
+}
---
+++ src/containers/Content/Content.js
@@ -0,0 +1,347 @@
+import React, { Component  } from 'react';
+import { Container } from 'theme/grid';
+import { Parallax } from 'react-scroll-parallax';
+import WhenInView from 'components/WhenInView/WhenInView';
+import {
+    OuterContainer,
+    SectionContainer,
+    Image,
+    ImageContainer,
+    IconsContainer,
+    Icon,
+    PhotoGallery,
+    Row,
+    PhotoContainerR,
+    PhotoContainerL,
+    MapContainer,
+    RevealTitle,
+    RevealText,
+    SportsCard,
+    ParallexText,
+} from './Content.style';
+import AmCharts from '@amcharts/amcharts3-react';
+import 'ammap3/ammap/ammap'
+
+export default class Content extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+    render() {
+        return (
+            <OuterContainer>
+                <ImageContainer id="home">
+                    <Image src={require('../../assets/images/landing-image-long.jpg')} alt="portrait picture" />
+                </ImageContainer>
+
+                <Container>
+
+                    <SectionContainer id="about">
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealTitle hide={!isInView}>
+                                    About Me
+                                    <hr />
+                                </RevealTitle>
+                            }
+                        </WhenInView>
+
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealText hide={!isInView}>
+                                    I am a 21 year old computer science student at the Rose Hulman Institute of technology.
+                                    I was born and raised in the vibrant city of La Paz, Bolivia. I am passionate about my
+                                    work, photograpy, travel, fitness and music. I built this site to share these things with you !
+                                </RevealText>
+                            }
+                        </WhenInView>
+
+                        <IconsContainer>
+                          <a href="https://www.youtube.com/playlist?list=PLFTQSQOjNIfNKpVekjwU3pzDtlZBKVYp3" target="_blank">
+                            <Icon src={require('../../assets/images/bolivia-map.png')} alt="Bolivia map" />
+                          </a>
+                          <a href="http://www.rose-hulman.edu/" target="_blank">
+                            <Icon src={require('../../assets/images/rose-hulman.png')} alt="Rose Hulman Icon" />
+                          </a>
+                        </IconsContainer>
+                    </SectionContainer>
+
+                    <SectionContainer id="sites">
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealTitle hide={!isInView}>
+                                    Websites
+                                    <hr />
+                                </RevealTitle>
+                            }
+                        </WhenInView>
+
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealText hide={!isInView}>
+                                    I love creating clean and beautiful sites from the ground up like this one. I enjoy designing,
+                                    developing and improving websites with top of the line technologies. I used React, Three.js, and
+                                    Firebase to develop this site. Here is a link to my portfolio site where you can see my other projects!
+                                </RevealText>
+                            }
+                        </WhenInView>
+
+                        <a href="https://anthonyderada.myportfolio.com/" target="_blank">
+                          <ImageContainer>
+                            <Image src={require('../../assets/images/PortfolioSite.png')} alt="portfolio-site" />
+                          </ImageContainer>
+                        </a>
+                    </SectionContainer>
+
+                    <SectionContainer id="photos">
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealTitle hide={!isInView}>
+                                    Photography
+                                    <hr />
+                                </RevealTitle>
+                            }
+                        </WhenInView>
+
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealText hide={!isInView}>
+                                    I recently picked up Photography as a hobby. Here are my favorite shots from last summer.
+                                </RevealText>
+                            }
+                        </WhenInView>
+
+                        <PhotoGallery>
+                          <Row>
+                            <PhotoContainerR>
+                              <Image src={require('../../assets/photos/CloudsTrain.JPG')} alt="portrait picture" />
+                            </PhotoContainerR>
+                            <PhotoContainerR>
+                              <Image src={require('../../assets/photos/MinuteToOne.JPG')} alt="portrait picture" />
+                            </PhotoContainerR>
+                            <PhotoContainerR>
+                              <Image src={require('../../assets/photos/SandHotelSunset.JPG')} alt="portrait picture" />
+                            </PhotoContainerR>
+                          </Row>
+                          <Row>
+                            <PhotoContainerL>
+                              <Image src={require('../../assets/photos/TheBeginning.JPG')} alt="portrait picture" />
+                            </PhotoContainerL>
+                            <PhotoContainerL>
+                              <Image src={require('../../assets/photos/TheEnd.jpg')} alt="portrait picture" />
+                            </PhotoContainerL>
+                          </Row>
+                          <Row>
+                            <PhotoContainerR>
+                              <Image src={require('../../assets/photos/BarcelonaCoast.JPG')} alt="portrait picture" />
+                            </PhotoContainerR>
+                            <PhotoContainerR>
+                              <Image src={require('../../assets/photos/Details.JPG')} alt="portrait picture" />
+                            </PhotoContainerR>
+                            <PhotoContainerR>
+                              <Image src={require('../../assets/photos/Perspective.JPG')} alt="portrait picture" />
+                            </PhotoContainerR>
+                          </Row>
+                        </PhotoGallery>
+                    </SectionContainer>
+
+                    <SectionContainer id="travel">
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealTitle hide={!isInView}>
+                                    Travel
+                                    <hr />
+                                </RevealTitle>
+                            }
+                        </WhenInView>
+
+                        <WhenInView>
+                            {({ isInView }) =>
+                                <RevealText hide={!isInView}>
+                                  I love traveling and visiting new countries !! Check out the map bellow with the countries that I have visited !
+                                </RevealText>
+                            }
+                        </WhenInView>
+
+                        <MapContainer>
+                          <AmCharts.React
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              margin: "auto",
+                            }}
+                            options={{
+                              "type": "map",
+                              "theme": "dark",
+                              "projection": "miller",
+                              "background-color" : "#ffffff",
+                              "dataProvider": {
+                                "map": "worldLow",
+                                "getAreasFromMap": false,
+                                "areas" :
+                                [
+                                  {
+                                    "id": "HR",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "CZ",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "DK",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "EE",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "FR",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "DE",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "GR",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "IT",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "LV",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "LI",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "LT",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "RU",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "ES",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "SE",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "GB",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "VA",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "CR",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "CW",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "DO",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "HT",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "JM",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "MX",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "PR",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "US",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "AR",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "BO",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "BR",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "CL",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "CO",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "PY",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "PE",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "UY",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "EG",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "ZM",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "ZA",
+                                    "showAsSelected": true
+                                  },
+                                  {
+                                    "id": "JP",
+                                    "showAsSelected": true
+                                  }
+                                ]
+                              },
+                              "areasSettings" : {
+                                "autoZoom" : true,
+                                "color" : "#ffffff",
+                                "colorSolid" : "#ffffff",
+                                "selectedColor" : "#C29E9D",
+                                "outlineColor" : "#dddddd",
+                                "rollOverColor" : "#ffffff",
+                                "rollOverOutlineColor" : "#777777"
+                              }
+                            }}
+                          />
+                        </MapContainer>
+
+                    </SectionContainer>
+                </Container>
+            </OuterContainer>
+        );
+    }
+}
